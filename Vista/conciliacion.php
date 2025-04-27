56dfd4f86b4354d0d18221a870e1f452c10e09dd
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-04-27 12:55:54 -0400

    Update conciliacion.php
    Actualización de la la vista de conciliación de cuentas

--- Vista/conciliacion.php
+++ Vista/conciliacion.php
@@ -3,13 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'header.php'; ?>
+    <link rel="stylesheet" href="Styles/darckort.css">
     <title>Consultar Cuentas</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>-->
 </head>
 
 <body>
+    <?php require_once("public/modal.php"); ?>
+    <?php include 'NavBar.php'; ?>
+
     <div class="container mt-5">
         <h2>Consultar Cuentas</h2>
         <button class="btn btn-primary mb-3" data-toggle="modal" data-target="#registrarModal">Registrar Cuenta</button>
@@ -112,6 +117,7 @@
         </div>
     </div>
 
+    <?php include 'footer.php'; ?>
     <script src="Javascript/conciliacion.js"></script>
 </body>
 </html>
