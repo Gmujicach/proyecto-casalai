89799771a1f15743d953b3a622ffe02ae967a450
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-04-27 12:59:24 -0400

    Update conciliacion.php

--- Vista/conciliacion.php
+++ Vista/conciliacion.php
@@ -6,9 +6,6 @@
     <?php include 'header.php'; ?>
     <link rel="stylesheet" href="Styles/darckort.css">
     <title>Consultar Cuentas</title>
-    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>-->
 </head>
 
 <body>
