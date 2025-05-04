8246eca13df0279a343f238e78421f2d7e11d20f
Darckort <darckortgame@gmail.com>
2025-05-04 13:36:48 -0400

    Avances de Tabla de consulta

--- Vista/Usuarios.php
+++ Vista/Usuarios.php
@@ -67,16 +67,17 @@ if (!isset($_SESSION['name'])) {
 
 
 <div class="contenedor-tabla">
-    <h3 class="titulo-tabla">LISTA DE USUARIOS</h3>
+    <h3>LISTA DE USUARIOS</h3>
 
     <table>
         <thead>
             <tr>
                 <th><input type="checkbox"></th>
                 <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Nombre de Usuario</th>
-                <th>Correo</th>
+                <th>Telefono</th>
+                <th>Rango</th>
+                <th>Estatus</th>
+                <th>Precio</th>
                 <th><i class="vertical">
                         <img src="IMG/more_opcion.svg" alt="Ícono" width="16" height="16">
                     </i>
@@ -88,10 +89,36 @@ if (!isset($_SESSION['name'])) {
         <?php foreach ($usuarios as $usuario): ?>
             <tr>
                 <td><input type="checkbox"></td>
-                <td><?php echo htmlspecialchars($usuario['nombres']); ?></td>
-                <td><?php echo htmlspecialchars($usuario['apellidos']); ?></td>
-                <td><?php echo htmlspecialchars($usuario['username']); ?></td>
-                <td><?php echo htmlspecialchars($usuario['correo']); ?></td>
+                <td>
+                    <span class="nombre-usuario">
+                    <?php echo htmlspecialchars($usuario['nombres']); ?> <?php echo htmlspecialchars($usuario['apellidos']); ?>
+                    </span>
+                    <span class="correo-usuario">
+                    <?php echo htmlspecialchars($usuario['correo']); ?>
+                    </span>
+                </td>
+                
+                <td>
+                    <span class="telefono-usuario">
+                    <?php echo htmlspecialchars($usuario['telefono']); ?>
+                    </span>
+                </td>
+                <td>
+                    <span class="rango-usuario">
+                    <?php echo htmlspecialchars($usuario['rango']); ?>
+                    </span>
+                </td>
+                <td>
+                    <span class="estatus-usuario <?php echo ($usuario['estatus'] == 'habilitado') ? 'habilitado' : 'inhabilitado'; ?>">
+                        <?php echo htmlspecialchars($usuario['estatus']); ?>
+                    </span>
+                </td>
+                <td>
+                    <span class="precio">200
+                    <!-- <//?php //echo htmlspecialchars($usuario['precio']); ?> -->
+                    </span>
+                    <span class="moneda">USD</span>
+                </td>
                 <td><a href="#" data-id="<?php echo htmlspecialchars($usuario['id_usuario']); ?>" class="btn btn-eliminar">Ver Mas</a></td>
                 <td>
                     <span>
@@ -110,6 +137,9 @@ if (!isset($_SESSION['name'])) {
         <?php endforeach; ?>
         </tbody>
     </table>
+
+
+
 
 <!-- Modal de modificación -->
     <div class="modal fade" id="modificar_usuario_modal" tabindex="-1" role="dialog" aria-labelledby="modificar_usuario_modal_label" aria-hidden="true">
@@ -158,112 +188,7 @@ if (!isset($_SESSION['name'])) {
 </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="table-container">
-    <h1 class="titulo-tabla display-5 text-center">LISTA DE USUARIOS</h1>
-    <table class="tabla">
-        <thead>
-            <tr>
-                <th>Acciones</th>
-                <th>Usuarios</th>
-                <th>Rango del Usuario</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($usuarios as $usuario): ?>
-                <tr>
-                    <td>
-                        <!-- Botón Modificar que abre el modal -->
-                        <button type="button" class="btn btn-modificar" data-toggle="modal" data-target="#modificarProductoModal" data-id="<?php echo htmlspecialchars($usuario['id_usuario']); ?>">
-                        Modificar
-                        </button>
-                        <br>
-                        <!-- Botón Eliminar -->
-                        <a href="#" data-id="<?php echo htmlspecialchars($usuario['id_usuario']); ?>" class="btn btn-eliminar">Eliminar</a>
-                    </td>
-                    <td><?php echo htmlspecialchars($usuario['username']); ?></td>
-                    <td><?php echo htmlspecialchars($usuario['rango']); ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-
-    
-
-<!-- Modal de modificación -->
-<div class="modal fade" id="modificar_usuario_modal" tabindex="-1" role="dialog" aria-labelledby="modificar_usuario_modal_label" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <form id="modificarusuario" method="POST" enctype="multipart/form-data">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modificar_usuario_modal_label">Modificar Usuario</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
-                <div class="modal-body">
-                    <!-- Campos del formulario de modificación -->
-                    <input type="hidden" id="modificar_id_usuario" name="id_usuario">
-                    <div class="form-group">
-                        <label for="modificarnombre_usuario">Nombre del Usuario</label>
-                        <input type="text" class="form-control" id="modificarnombre_usuario" name="nombre_usuario" maxlength="15" required>
-                        <span id="smodificarnombre_usuario"></span>
-                    </div>
-                    <div class="form-group">
-                        <label for="modificarclave_usuario">Contraseña del Usuario</label>
-                        <input type="text" class="form-control" id="modificarclave_usuario" name="clave_usuario" required>
-                        <span id="smodificarclave_usuario"></span>
-                    </div>
-                    <div class="form-group col-md-4">
-                                    <label for="rango">Categorias</label>
-                                    <select class="custom-select" id="rango" name="rango">
-                                    <option value="USUARIO">Usuario</option>
-                                                        <option value="admin">Administrador</option>
-                                                        <option value="almacen">Almacen</option>    
-                                    </select>
-                                </div>
-                    
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-cerrar" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="submit" class="btn btn-primary">Modificar</button>
-                </div>
-                </div>
-                
-            </form>
-        </div>
-    </div>
-    <div class="containera"> <!-- todo el contenido ira dentro de esta etiqueta-->
-
-<form method="post" action="" id="f" target="_blank">
-<div class="containera">
-    <div class="row">
-        <div class="col">
-               <button type="button" class="btn btn-primary" id="pdfusuarios" name="pdfusuarios"><a href="?pagina=pdfusuarios">GENERAR REPORTE</button>
-        </div>
-        
-    </div>
-</div>
-</form>
-    
-</div> <!-- fin de container -->
-</div>
-
+<!-- Modal de eliminación -->
 
 <script src="public/bootstrap/js/sidebar.js"></script>
   <script src="public/bootstrap/js/bootstrap.bundle.min.js"></script>
