4fa5918d23e4ad7cf552bcc4d8858c0cbbc487a6
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-05-10 15:18:24 -0400

    create de BD de seguridad

--- BD/seguridad.sql
+++ BD/seguridad.sql
@@ -17,9 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `seguridad`
---
+-- Creación de la base de datos: `seguridad`
+
+CREATE DATABASE IF NOT EXISTS `seguridad` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `seguridad`;
 
 -- --------------------------------------------------------
 
