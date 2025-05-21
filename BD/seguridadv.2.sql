81b4983c87b6f26679b7971f89248558e86a8e8a
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-05-20 21:45:59 -0400

    actualizacion de BD de seguridad

--- BD/seguridadv.2.sql
+++ BD/seguridadv.2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-05-2025 a las 04:03:49
+-- Tiempo de generación: 21-05-2025 a las 03:44:57
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -17,21 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
--- Creación de la base de datos: `seguridad_cl`
-
-CREATE DATABASE IF NOT EXISTS `seguridad_cl` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `seguridad_cl`;
-
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `tbl_modulos`
+-- Base de datos: `seguridad_cl`
 --
-
-CREATE TABLE `tbl_modulos` (
-  `id_modulo` int(11) NOT NULL,
-  `nombre_modulo` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -64,6 +52,17 @@ CREATE TABLE `tbl_bitacora` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `tbl_modulos`
+--
+
+CREATE TABLE `tbl_modulos` (
+  `id_modulo` int(11) NOT NULL,
+  `nombre_modulo` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tbl_permisos`
 --
 
@@ -81,7 +80,7 @@ CREATE TABLE `tbl_permisos` (
 
 CREATE TABLE `tbl_rango` (
   `id_rango` int(11) NOT NULL,
-  `rango` varchar(15) NOT NULL
+  `nombre_rango` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -107,7 +106,7 @@ CREATE TABLE `tbl_usuarios` (
   `id_usuario` int(11) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `rango` varchar(20) NOT NULL,
+  `id_rango` int(11) NOT NULL,
   `correo` varchar(50) DEFAULT NULL,
   `nombres` varchar(20) DEFAULT NULL,
   `apellidos` varchar(20) DEFAULT NULL,
@@ -115,24 +114,8 @@ CREATE TABLE `tbl_usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `tbl_usuarios`
---
-
-INSERT INTO `tbl_usuarios` (`id_usuario`, `username`, `password`, `rango`, `correo`, `nombres`, `apellidos`, `telefono`) VALUES
-(0, 'Diego', '0510', 'almacen', NULL, NULL, NULL, NULL),
-(1, 'Admin', '12345', 'Administrador', 'ejemplo@gmail.com', 'Administrador', 'Administrador', '04145753363'),
-(2, 'Despachador', '12345', 'Despachador', 'ejemplo@gmail.com', 'Despachador', 'Despachador', '04145753363'),
-(3, 'Cliente', '12345', 'Cliente', 'ejemplo@gmail.com', 'Cliente', 'Cliente', '04145753363');
-
---
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `tbl_modulos`
---
-ALTER TABLE `tbl_modulos`
-  ADD PRIMARY KEY (`id_modulo`);
 
 --
 -- Indices de la tabla `tbl_alertas`
@@ -148,6 +131,12 @@ ALTER TABLE `tbl_bitacora`
   ADD PRIMARY KEY (`id_bitacora`),
   ADD KEY `id_modulo` (`id_modulo`,`id_usuario`),
   ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `tbl_modulos`
+--
+ALTER TABLE `tbl_modulos`
+  ADD PRIMARY KEY (`id_modulo`);
 
 --
 -- Indices de la tabla `tbl_permisos`
@@ -172,17 +161,12 @@ ALTER TABLE `tbl_recuperar`
 -- Indices de la tabla `tbl_usuarios`
 --
 ALTER TABLE `tbl_usuarios`
-  ADD PRIMARY KEY (`id_usuario`);
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_rango` (`id_rango`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `tbl_modulos`
---
-ALTER TABLE `tbl_modulos`
-  MODIFY `id_modulo` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_alertas`
@@ -195,6 +179,12 @@ ALTER TABLE `tbl_alertas`
 --
 ALTER TABLE `tbl_bitacora`
   MODIFY `id_bitacora` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_modulos`
+--
+ALTER TABLE `tbl_modulos`
+  MODIFY `id_modulo` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_rango`
@@ -231,6 +221,12 @@ ALTER TABLE `tbl_permisos`
 --
 ALTER TABLE `tbl_recuperar`
   ADD CONSTRAINT `tbl_recuperar_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `tbl_usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  ADD CONSTRAINT `fk_usuarios_rango` FOREIGN KEY (`id_rango`) REFERENCES `tbl_rango` (`id_rango`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
