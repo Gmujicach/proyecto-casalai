250af9141c9fcf348ab2d1842b4c29f61e2e571e
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-05-24 09:53:47 -0400

    Update config.php

--- Modelo/config.php
+++ Modelo/config.php
@@ -1,28 +1,20 @@
 <?php 
-
 class BD{
+	private $host;
+	private $base;
+	private $usu;
+	private $clave;
 
-		private $host;
-		private $base;
-		private $usu;
-		private $clave;
-
-		function __construct(){
-
-			$this->host = 'localhost';
-			$this->base = 'casalai';
-			$this->usu = 'root';
-			$this->clave = '';
-
-		}
-
-		function conexion(){
-
-			$conex = new PDO('mysql:host='.$this->host.';dbname='.$this->base.';charset=utf8',$this->usu,$this->clave);
-
-			return $conex;
-
-		}
+	function __construct(){
+		$this->host = 'localhost';
+		$this->base = 'casalai';
+		$this->usu = 'root';
+		$this->clave = '';
 	}
 
+	function conexion(){
+		$conex = new PDO('mysql:host='.$this->host.';dbname='.$this->base.';charset=utf8',$this->usu,$this->clave);
+		return $conex;
+	}
+}
 ?>
