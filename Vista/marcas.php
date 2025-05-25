d7dc5529962347bafbb3b98d3726c3f2148f2516
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-05-25 18:33:33 -0400

    Update marcas.php

--- Vista/marcas.php
+++ Vista/marcas.php
@@ -1,22 +1,17 @@
 <?php
-
-
-
 if (!isset($_SESSION['name'])) {
-
- 	header('Location: .');
- 	exit();
- }
+    header('Location: .');
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Gestionar Marcas</title>
-  <?php include 'header.php'; ?>
-  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestionar Marcas</title>
+    <?php include 'header.php'; ?>
 </head>
 <body>
 
