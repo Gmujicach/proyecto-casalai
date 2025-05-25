69cf335298b75900ee210ccf62e2abdeec51bd8d
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-05-25 18:33:48 -0400

    Update cuentas.php

--- Vista/cuentas.php
+++ Vista/cuentas.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['name'])) {
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestionar Cuentas Bancarias</title>
     <?php include 'header.php'; ?>
