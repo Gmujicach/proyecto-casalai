bc2caac702f7ef824c28a49c7a424a75ea4b9db4
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-05-27 01:18:51 -0400

    creacion BDs

--- BD/segurdadlaiv.3.sql
+++ BD/segurdadlaiv.3.sql
@@ -17,9 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `segurdadlai`
---
+-- Creación de la base de datos: `casalai`
+
+CREATE DATABASE IF NOT EXISTS `segurdadlai` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `segurdadlai`;
 
 -- --------------------------------------------------------
 
