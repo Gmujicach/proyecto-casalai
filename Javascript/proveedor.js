b54443f9d8c02fa3050b5c6d0583c2cf2eecfbcc
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-06-17 20:13:26 -0400

    Update proveedor.js

--- Javascript/proveedor.js
+++ Javascript/proveedor.js
@@ -632,7 +632,7 @@ $(document).ready(function () {
         return errores;
     }
 
-    $(document).on('click', '#modificarProveedorbtn', function () {
+    $('#modificarProveedorbtn').on('click', function() {
         $('#modificar_id_proveedor').val($(this).data('id'));
         $('#modificar_nombre_proveedor').val($(this).data('nombre-proveedor'));
         $('#modificar_rif_proveedor').val($(this).data('rif-proveedor'));
