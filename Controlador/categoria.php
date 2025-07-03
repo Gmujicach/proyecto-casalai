252cd6c6a29ef9b9a05e3667dbf540e81db12dc5
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-07-03 02:43:37 -0400

    Update categoria.php

--- Controlador/categoria.php
+++ Controlador/categoria.php
@@ -7,10 +7,6 @@ require_once 'Modelo/Bitacora.php';
 $id_rol = $_SESSION['id_rol']; // Asegúrate de tener este dato en sesión
 
 define('MODULO_CATEGORIA', 1);
-define('ACCION_CREAR', 1);
-define('ACCION_ACTUALIZAR', 3);
-define('ACCION_ELIMINAR', 4);
-define('ACCION_CAMBIAR_ESTATUS', 5);
 
 $permisosObj = new Permisos();
 $bitacoraModel = new Bitacora();
@@ -40,9 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $bitacoraModel->registrarAccion(
                     'Creación de categoria: ' . $_POST['nombre_categoria'], 
                     MODULO_CATEGORIA, 
-                    $id_usuario_accion,
-                    ACCION_CREAR,
-                    $categoriaRegistrado['id_usuario']
+                    $_SESSION['id_usuario']
                 );
 
                 echo json_encode([
@@ -107,9 +101,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $bitacoraModel->registrarAccion(
                     'Actualización de categoria: ' . $_POST['nombre_categoria'], 
                     MODULO_CATEGORIA, 
-                    $id_usuario_accion,
-                    ACCION_ACTUALIZAR,
-                    $categoriaActualizada['id_usuario']
+                    $_SESSION['id_usuario']
                 );
 
                 echo json_encode([
@@ -127,11 +119,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $resultado = $categoria->eliminarCategoria($id_categoria);
             if (is_array($resultado) && isset($resultado['status']) && $resultado['status'] === 'error') {
                 $bitacoraModel->registrarAccion(
-                    'Eliminación de categoria: ' . $resultado['nombre_categoria'], 
+                    'Eliminación de categoria: (ID: ' . $id_categoria . ')', 
                     MODULO_CATEGORIA, 
-                    $id_usuario_accion,
-                    ACCION_ELIMINAR,
-                    $id_categoria
+                    $_SESSION['id_usuario']
                 );
                 echo json_encode(['status' => 'error', 'message' => $resultado['mensaje']]);
             } else {
