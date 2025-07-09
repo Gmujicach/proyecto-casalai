2426a40054a6452166dee67446ec748afb62a225
Gmujicach <126182278+Gmujicach@users.noreply.github.com>
2025-07-09 19:42:29 -0400

    Update Proveedores.php
    Corregido la ruta del archivo bitácora.
    Antes: "require_once 'Modelo/Bitacora'"
    Ahora: "require_once 'Modelo/bitacora'"

--- Controlador/Proveedores.php
+++ Controlador/Proveedores.php
@@ -4,7 +4,7 @@ ob_start();
 require_once 'Modelo/Proveedores.php';
 require_once 'Modelo/Productos.php';
 require_once 'Modelo/Permisos.php';
-require_once 'Modelo/Bitacora.php';
+require_once 'Modelo/bitacora.php';
 
 $id_rol = $_SESSION['id_rol']; // Asegúrate de tener este dato en sesión
 
